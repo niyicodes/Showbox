a244662e922ca525f95ff2619b73eca17618d906
niyicodes <elijahtost@gmail.com>
2023-05-04 18:50:37 +0100

    checkinf if the cast card on ios is good

--- src/Components/Cast.jsx
+++ src/Components/Cast.jsx
@@ -1,7 +1,7 @@
 const Cast = ({ image, name, act_name }) => {
  return (
   <div className="w-[200px] overflow-hidden shadow-xl rounded-b-lg">
-   <img src={image} alt="image" className="rounded-t-xl h-3/4 object-cover"/>
+   <img src={image} alt="image" className="rounded-t-xl w-[210px] h-3/4 object-cover"/>
    <div className="mt-3 text-center">
    <h4 className="text-xl font-bold">{name}</h4>
    <p className="text-base text-slate-400">{act_name}</p>
